175d1dd80d7b66bacb0a66807449af8a87f9d80a
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-03-20 16:46:44 +0530

    Added code for data types

---
+++ src/javabasics/DataTypes.java
@@ -0,0 +1,21 @@
+package javabasics;
+
+public class DataTypes {
+
+    public static void main(String args[]) {
+        System.out.println("learning java data types");
+        byte x = 2;
+        System.out.println(x);
+        int age = 10;
+        System.out.println(age);
+        float rateOfInterest = 2.3f;
+        System.out.println(rateOfInterest);
+        double rateOfNewValue = 2.33;
+        System.out.println(rateOfNewValue);
+        boolean isThisSeries = false;
+        System.out.println(isThisSeries);
+        char myCharacter = 'a';
+        System.out.println(myCharacter);
+
+    }
+}
---
+++ src/javabasics/Main.java
@@ -0,0 +1,8 @@
+package javabasics;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Learning java basics");
+    }
+}
