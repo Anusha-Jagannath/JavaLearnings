b79b1630af5b71a88fc0786073e77e821644a5e3
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-03-21 22:34:31 +0530

    Added code for datatyes

---
+++ src/datatypes/Main.java
@@ -0,0 +1,35 @@
+package datatypes;
+
+public class Main {
+
+    public static void main(String args[]) {
+       System.out.println("Learning java basics");
+
+       byte d = 127;
+       System.out.println(d);
+
+       short s = 128;
+       System.out.println(s);
+
+       int a = 2;
+       System.out.println(a);
+
+       float b = 2.3f;
+       System.out.println(b);
+
+       double c = 2.3;
+       System.out.println(c);
+
+       char character = 'a';
+       System.out.println(character);
+
+       String string = "ghjk";
+       System.out.println(string);
+
+       boolean bo = false;
+       System.out.println(bo);
+
+
+
+    }
+}
