260d3eab7a0cf60e34654067bcd67fa5e5f55986
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-03-22 11:40:00 +0530

    Added code for type casting

---
+++ src/typecasting/Main.java
@@ -0,0 +1,22 @@
+package typecasting;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Learning type casting");
+
+        byte x = 4;
+        int a = x;
+        System.out.println(a);
+
+        int i = 4;
+        double d = i;
+        System.out.println(d);
+
+        double myDouble = 3.84;
+        int myInt = (int) myDouble;
+
+        System.out.println(myInt);
+
+    }
+}
