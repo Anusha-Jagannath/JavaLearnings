6007eaa0a4f5b449e9e52e15c2e21332d983d914
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-04-25 22:32:43 +0530

    Added: code for encapsulation in java

---
+++ src/encapsulation/Main.java
@@ -0,0 +1,20 @@
+package encapsulation;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        System.out.println("Learning encapsulation");
+        Student student = new Student("anu");
+        Teacher teacher = new Teacher("anu");
+
+       // student.name;
+
+        Scanner sc = new Scanner(System.in);
+        
+
+
+    }
+}
