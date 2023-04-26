550ead3c08aaab85a15dc71191219a3d99e59a88
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-04-26 23:00:48 +0530

    Added: code for encapsulation

---
+++ src/accessspecifier/Teacher.java
@@ -0,0 +1,19 @@
+package accessspecifier;
+
+public class Teacher {
+
+
+    String name;
+
+    public Teacher(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
