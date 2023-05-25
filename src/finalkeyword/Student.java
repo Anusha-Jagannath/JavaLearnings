f9e7fab9164d3784d28e5df7eb56da3249d3e4d5
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-05-25 23:29:10 +0530

    Added code for final keyword

---
+++ src/finalkeyword/Student.java
@@ -0,0 +1,12 @@
+package finalkeyword;
+
+public class Student {
+
+    final int rollNo;
+    final String name;
+
+    public Student(int rollNo,String name) {
+        this.name = name;
+        this.rollNo = rollNo;
+    }
+}
