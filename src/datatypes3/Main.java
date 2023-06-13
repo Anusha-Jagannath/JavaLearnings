562b7ae638a065d6bd86f2696bedde235539cc04
Anusha-Jagannath <anushajaganath6@gmail.com>
2023-06-13 22:07:44 +0530

    Added: tyoes in java

--- src/datatypes3/Main.java
+++ src/datatypes3/Main.java
@@ -4,6 +4,25 @@ public class Main {
 
     public static void main(String args[]) {
         System.out.println("Learning java basics");
+        byte x = 127;
+        System.out.println(x);
+
+        int y = 2;
+        System.out.println(y);
+
+        short s = 4;
+        System.out.println(s);
+
+        int age = 10;
+        float rateOfInterest = 2.3f;
+
+        boolean isThis = false;
+        System.out.println(isThis);
+
+        char myCharacter = 'a';
+        System.out.println(myCharacter);
+
+
 
     }
 }
